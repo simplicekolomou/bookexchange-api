7e4d7edeaea4d47f566047fc0b2ef65a941d29f8
m.bragard@student.helmo.be <mathieubragard8@gmail.com>
2025-10-27 21:39:52 +0100

    projet généré par spring initializer

---
+++ src/test/java/ovh/bookexchange/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ovh.bookexchange.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
