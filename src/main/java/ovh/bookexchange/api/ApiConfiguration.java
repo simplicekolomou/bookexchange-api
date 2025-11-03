ed515529843c1992c526a150c29c477fbef8aeb3
Bragard Mathieu <m.bragard@student.helmo.be>
2025-11-03 12:28:51 +0100

    configure jpa

---
+++ src/main/java/ovh/bookexchange/api/ApiConfiguration.java
@@ -0,0 +1,48 @@
+package ovh.bookexchange.api;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories
+@EnableTransactionManagement
+public class ApiConfiguration {
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder dsBuilder = DataSourceBuilder.create();
+        dsBuilder.url("jdbc:postgresql://localhost:5432/postgres");
+        dsBuilder.driverClassName("org.postgresql.Driver");
+        dsBuilder.username("postgres");
+        dsBuilder.password("password");
+        return dsBuilder.build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("ovh.bookexchange.api.domains.entities");
+        factory.setDataSource(dataSource());
+        return factory;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory);
+        return txManager;
+    }
+}
