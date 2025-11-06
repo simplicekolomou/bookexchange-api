192362dc94bb5e4f84e8527eb7b9091b9f44b6d0
Bragard Mathieu <m.bragard@student.helmo.be>
2025-11-06 14:34:20 +0100

    env variables

--- src/main/java/ovh/bookexchange/api/ApiConfiguration.java
+++ src/main/java/ovh/bookexchange/api/ApiConfiguration.java
@@ -1,9 +1,11 @@
 package ovh.bookexchange.api;
 
 import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -17,20 +19,23 @@ import javax.sql.DataSource;
 @EnableJpaRepositories
 @EnableTransactionManagement
 public class ApiConfiguration {
+    @Autowired
+    private Environment environment;
+
     @Bean
     public DataSource dataSource() {
         DataSourceBuilder dsBuilder = DataSourceBuilder.create();
-        dsBuilder.url("jdbc:postgresql://localhost:5432/postgres");
-        dsBuilder.driverClassName("org.postgresql.Driver");
-        dsBuilder.username("postgres");
-        dsBuilder.password("password");
+        dsBuilder.url(environment.getProperty("spring.datasource.url", "jdbc:postgresql://localhost:5432/postgres"));
+        dsBuilder.driverClassName(environment.getProperty("spring.datasource.driver-class-name","org.postgresql.Driver"));
+        dsBuilder.username(environment.getProperty("spring.datasource.username", "postgres"));
+        dsBuilder.password(environment.getProperty("spring.datasource.password", "password"));
         return dsBuilder.build();
     }
 
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
         HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        vendorAdapter.setGenerateDdl(true);
+        vendorAdapter.setGenerateDdl(environment.getProperty("spring.jpa.hibernate.ddl-auto", Boolean.class, false));
 
         LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
         factory.setJpaVendorAdapter(vendorAdapter);
