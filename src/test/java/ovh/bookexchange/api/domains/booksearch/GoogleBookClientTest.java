7b646b9f678b8586c42c2774feb9d4bdbe8be744
Bragard Mathieu <m.bragard@student.helmo.be>
2025-11-20 12:43:44 +0100

    remove broken import

--- src/test/java/ovh/bookexchange/api/domains/booksearch/GoogleBookClientTest.java
+++ src/test/java/ovh/bookexchange/api/domains/booksearch/GoogleBookClientTest.java
@@ -1,7 +1,6 @@
 package ovh.bookexchange.api.domains.booksearch;
 
 import graphql.Assert;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
