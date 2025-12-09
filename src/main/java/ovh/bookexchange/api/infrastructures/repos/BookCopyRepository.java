6ddaab1c7fb36f88a8b81d169f8e9fec6998a0ae
Theo A. <t.andernack@student.helmo.be>
2025-12-09 09:29:21 +0100

    Correction warnings

--- src/main/java/ovh/bookexchange/api/infrastructures/repos/BookCopyRepository.java
+++ src/main/java/ovh/bookexchange/api/infrastructures/repos/BookCopyRepository.java
@@ -3,7 +3,6 @@ package ovh.bookexchange.api.infrastructures.repos;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.ListPagingAndSortingRepository;
-import ovh.bookexchange.api.domains.entities.AvailabilityType;
 import ovh.bookexchange.api.domains.entities.BookCopy;
 
 import java.util.List;
@@ -11,8 +10,6 @@ import java.util.Optional;
 
 public interface BookCopyRepository  extends ListPagingAndSortingRepository<BookCopy, Long>, CrudRepository<BookCopy, Long> {
     List<BookCopy> findByOwnerEmail(String email, Pageable pageable);
-
-    List<BookCopy> findByOwnerEmailAndIdIs(String ownerEmail, long id, Pageable pageable);
 
     Optional<BookCopy> findByIdAndOwnerId(long id, long ownerId);
 
