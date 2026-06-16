547bb35b0fd77f347eb09f8324c23730f4ff903d
simplicekolomou <s.kolomou@student.helmo.be>
2026-06-16 03:43:04 +0200

    ajout configuration cors pour le déploiement

--- src/main/java/ovh/bookexchange/api/configurations/SecurityConfiguration.java
+++ src/main/java/ovh/bookexchange/api/configurations/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package ovh.bookexchange.api.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -48,6 +49,7 @@ public class SecurityConfiguration {
         return http.cors(withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/", "/login", "/register", "/reset-password", "/forgot-password").permitAll()
                         .requestMatchers("/error").permitAll()
                         .requestMatchers("/ws/**").permitAll()
