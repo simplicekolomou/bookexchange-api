f5a16dfe2eb59e1e008a4d5909579450b382ccbc
simplicekolomou <s.kolomou@student.helmo.be>
2026-06-19 22:01:37 +0200

    correction de l'initialisation de firebase

--- src/main/java/ovh/bookexchange/api/configurations/FirebaseConfig.java
+++ src/main/java/ovh/bookexchange/api/configurations/FirebaseConfig.java
@@ -3,7 +3,7 @@ package ovh.bookexchange.api.configurations;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
@@ -12,19 +12,16 @@ import java.io.InputStream;
 @Configuration
 public class FirebaseConfig {
 
-    @PostConstruct
-    public void initializeFirebase() throws Exception {
-        // Éviter une double initialisation
+    @Bean
+    public FirebaseApp firebaseApp() throws Exception {
         if (FirebaseApp.getApps().isEmpty()) {
-            // Charger le fichier depuis le classpath (src/main/resources)
             InputStream serviceAccount = new ClassPathResource("firebase-service-account.json").getInputStream();
-
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
-
-            FirebaseApp.initializeApp(options);
-            System.out.println("✅ FirebaseApp initialisée avec succès !");
+            return FirebaseApp.initializeApp(options);
+        } else {
+            return FirebaseApp.getInstance();
         }
     }
 }
